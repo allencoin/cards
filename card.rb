cff179927194f6fe3c0edec490428cf9dd7d5fce
Allen Coin <allencoin@gmail.com>
2015-04-20 09:20:46 +0000

    Initial commit

---
+++ card.rb
@@ -0,0 +1,53 @@
+class Card
+	attr_accessor :rank, :suit
+
+	def initialize(rank, suit)
+		@rank = rank
+		@suit = suit
+	end
+
+	def output_card
+		puts "#{self.rank} of #{self.suit}"
+	end
+
+	# def self.random_card
+	# 	Card.new(rand(10), :spades)
+	# end
+end
+
+class Deck
+	def initialize
+		@cards = []
+		# @cards << Card.new(10, :spades)
+		# @cards << Card.new(9, :diamonds)
+		# I didn't like the idea of typing out 52 cards
+		# with the above method so I opted to build them
+		# by looping through 2 arrays.
+		ranks = %w{Ace 2 3 4 5 6 7 8 9 10 Jack Queen King}
+		suits = %w{Spades Hearts Diamonds Clubs}
+			suits.each do |suit|
+  				ranks.size.times do |i|
+    				@cards << Card.new( ranks[i], suit )
+  				end
+			end
+	end
+
+	def shuffle
+		@cards.shuffle!
+	end
+
+	def output
+		@cards.each do |card|
+			card.output_card
+		end
+	end
+
+	def deal
+		@cards.shift.output_card
+	end
+end
+
+deck = Deck.new
+deck.shuffle
+deck.output
+deck.deal
